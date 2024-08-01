c64453148c8228d874f8a947cb7bdb662df22681
afkimuhafaf <402150mfaf@gmail.com>
2024-08-01 22:26:03 +0700

    fix

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,12 @@
 function validateForm() {
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-}
+    const name = document.getElementById("customer-name").value;
+    const email = document.getElementById("customer-email").value;
 
     if (name === "" || email === "") {
         alert  ("Tidak boleh kosong");
-        return false;
 }
 
     if (!email.includes("@")) {
         alert  ("Email harus mengandung @");
-        return false;
 }
-
-document.getElementById("submit").addEventListener("click", function(){
-    return validateForm();
-})
-
+}
